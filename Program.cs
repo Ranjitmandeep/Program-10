0baa21a181e7af7db104fd46fbe35def42bd70ec
Mandeep Ranjit <Mandeep Ranjit@LAPTOP-SMV5472A>
2019-05-27 09:06:27 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_10
+{
+    class Program
+    // This program will display the patteren of right angle using asterisk.
+    {
+        static void Main(string[] args)
+        {
+            int a, b, rows;
+            Console.Write("\n\n");
+            Console.Write("Display the pattern like right angle using asterisk:\n");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (a = 1; a <= rows; a++)
+            {
+                for (b = 1; b <= a; b++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+}
